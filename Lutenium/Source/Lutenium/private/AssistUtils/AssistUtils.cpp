e11a94d302acce6455c20ea1a46cb18cb1e010f2
Misha <homakhaker@gmail.com>
2020-07-24 17:55:06 +0200

    Added assistnace for missile's target

--- Lutenium/Source/Lutenium/private/AssistUtils/AssistUtils.cpp
+++ Lutenium/Source/Lutenium/private/AssistUtils/AssistUtils.cpp
@@ -1,19 +1,56 @@
 ﻿#include "../../public/AssistUtils/AssistUtils.h"
 #include "../../public/PlaneMovementComponent.h"
 #include "Math/Vector.h"
+#include "Kismet/KismetSystemLibrary.h"
 
 bool FAssistUtils::bHasApplied = false;
 
-void FAssistUtils::ApplyTakeOffAcceleration(UPlaneMovementComponent* PlaneMovementComponent, const FVector Velocity,
-                                           const float MinTakeOffVelocity, float AddedAcceleration)
+void FAssistUtils::ApplyTakeOffAcceleration(UPlaneMovementComponent* PlaneMovementComponent, const FVector& Velocity,
+                                            const float MinTakeOffVelocity, const float AddedAcceleration)
 {
-   if(!FAssistUtils::bHasApplied && Velocity.Size() < MinTakeOffVelocity)
-   {      
-       PlaneMovementComponent->AddAcceleration(AddedAcceleration);
-       bHasApplied = true;
-   }
-   else if(Velocity.Size() > MinTakeOffVelocity){
+    if (!FAssistUtils::bHasApplied && Velocity.Size() < MinTakeOffVelocity)
+    {
+        PlaneMovementComponent->AddAcceleration(AddedAcceleration);
+        bHasApplied = true;
+    }
+    else if (Velocity.Size() > MinTakeOffVelocity)
+    {
         FAssistUtils::bHasApplied = false;
-   }
+    }
 }
 
+FVector FAssistUtils::RaycastMissileTarget(const UWorld* World, const FVector& StartLocation,
+                                           const FVector& ForwardVector)
+{
+    if (World)
+    {
+        FHitResult FirstHitResult;
+
+        const FVector EndLocation = StartLocation + ForwardVector * RaycastMissileTarget_Length;
+        const bool bFirstHit = World->SweepSingleByChannel(FirstHitResult,
+                                                           StartLocation,
+                                                           EndLocation,
+                                                           FQuat::Identity,
+                                                           ECollisionChannel::ECC_GameTraceChannel1,
+                                                           FCollisionShape::MakeSphere(RaycastMissile_First_Radius));
+        if (bFirstHit)
+        {
+            return FirstHitResult.Location;
+        }
+
+        FHitResult SecondHitResult;
+        const bool bSecondHit = World->SweepSingleByChannel(SecondHitResult,
+                                                            StartLocation,
+                                                            EndLocation,
+                                                            FQuat::Identity,
+                                                            ECollisionChannel::ECC_GameTraceChannel1,
+                                                            FCollisionShape::MakeSphere(RaycastMissile_Second_Radius));
+        if (bSecondHit)
+        {
+            return SecondHitResult.Location;
+        }
+
+        return EndLocation;
+    }
+    return FVector();
+}
--- Lutenium/Source/Lutenium/public/AssistUtils/AssistUtils.h
+++ Lutenium/Source/Lutenium/public/AssistUtils/AssistUtils.h
@@ -6,8 +6,16 @@ struct FVector;
 class FAssistUtils
 {
 public:
-    static void ApplyTakeOffAcceleration(UPlaneMovementComponent* PlaneMovementComponent,
-                                         FVector Velocity, float MinTakeOffVelocity, float AddedAcceleration);
+    static void ApplyTakeOffAcceleration(UPlaneMovementComponent* PlaneMovementComponent, const FVector& Velocity,
+                                           const float MinTakeOffVelocity, const float AddedAcceleration);
+
+    static FVector RaycastMissileTarget(const UWorld* World,const FVector& StartLocation, const FVector& ForwardVector);
 private:
     static bool bHasApplied;
+
+    const static float RaycastMissileTarget_Length = 500000.f;
+
+    const static float RaycastMissile_First_Radius = 750.f;
+
+    const static float RaycastMissile_Second_Radius = 1500.f;
 };
