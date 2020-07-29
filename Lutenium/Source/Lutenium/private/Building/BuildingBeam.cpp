88bb9dd1911528b82d8f61abbe4e81e17dd82117
Misha <homakhaker@gmail.com>
2020-07-29 21:13:54 +0200

    working on the lods and performance overall

--- Lutenium/Source/Lutenium/private/Building/BuildingBeam.cpp
+++ Lutenium/Source/Lutenium/private/Building/BuildingBeam.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Components/StaticMeshComponent.h"
 #include "../../public/Building/BuildingBeam.h"
+#include "Components/StaticMeshComponent.h"
 
 // Sets default values
 ABuildingBeam::ABuildingBeam() {
@@ -11,7 +11,6 @@ ABuildingBeam::ABuildingBeam() {
 	PlatformStart = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PlatformStart"));
 	RootComponent = PlatformStart;
 	PlatformEnd = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PlatformEnd"));
-	bRunConstructionScriptOnDrag = false;
 	Points = 2;
 }
 
