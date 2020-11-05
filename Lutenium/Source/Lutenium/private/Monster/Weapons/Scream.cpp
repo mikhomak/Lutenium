78a9eed0ead0a05b67c3de62c885d699997c60e3
Misha <homakhaker@gmail.com>
2020-11-05 21:17:27 +0100

    reworking scream lel

--- Lutenium/Source/Lutenium/private/Monster/Weapons/Scream.cpp
+++ Lutenium/Source/Lutenium/private/Monster/Weapons/Scream.cpp
@@ -14,19 +14,16 @@ AScream::AScream()
     MainSphereComp = CreateDefaultSubobject<USphereComponent>(TEXT("Main Sphere"));
     RootComponent = MainSphereComp;
 
-    FirstWaveMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("First Wave"));
-    FirstWaveMesh->AttachToComponent(MainSphereComp, FAttachmentTransformRules::KeepWorldTransform);
-    FirstWaveMesh->SetGenerateOverlapEvents(true);
-    SecondWaveMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Second Wave"));
-    SecondWaveMesh->AttachToComponent(MainSphereComp, FAttachmentTransformRules::KeepWorldTransform);
-    SecondWaveMesh->SetGenerateOverlapEvents(true);
+    WaveMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Wave Mesh"));
+    WaveMesh->AttachToComponent(MainSphereComp, FAttachmentTransformRules::KeepWorldTransform);
+    WaveMesh->SetGenerateOverlapEvents(true);
+
     WavesLifeSpan = 10.f;
     ScalingSpeed = 1.01f;
-    SecondFaveDelay = 1.f;
     ScalingMultiplier = FVector(1.f, 1.f, 0.f);
 
-    FirstWaveForceImpact = 9000.f;
-    FirstWaveDragForce = 90000.f;
+    ForceImpact = 9000.f;
+    DragForce = 90000.f;
 }
 
 void AScream::BeginPlay()
@@ -34,72 +31,47 @@ void AScream::BeginPlay()
     Super::BeginPlay();
     /* Add overlap function calls in blueprints! */
     SetLifeSpan(WavesLifeSpan);
-    FTimerHandle SecondWaveDelayTimerHandler;
-    GetWorld()->GetTimerManager().SetTimer(SecondWaveDelayTimerHandler, this, &AScream::SecondWaveDelayStart,
-                                           SecondFaveDelay,
-                                           false);
 }
 
 void AScream::Tick(float DeltaTime)
 {
     Super::Tick(DeltaTime);
 
-    const FVector FirstWaveCurrentScale = FirstWaveMesh->GetComponentScale();
+    const FVector FirstWaveCurrentScale = WaveMesh->GetComponentScale();
     const FVector FirstWaveNewScale = FirstWaveCurrentScale + ScalingMultiplier * ScalingSpeed;
-    FirstWaveMesh->SetWorldScale3D(FirstWaveNewScale);
-    if (DidSecondWaveStart)
-    {
-        const FVector SecondWaveCurrentScale = SecondWaveMesh->GetComponentScale();
-        const FVector SecondWaveNewScale = SecondWaveCurrentScale + ScalingMultiplier * ScalingSpeed;
-        SecondWaveMesh->SetWorldScale3D(SecondWaveNewScale);
-    }
+    WaveMesh->SetWorldScale3D(FirstWaveNewScale);
 }
 
-void AScream::FirstWaveOverlap(
-    UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
-    UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
-    bool bFromSweep, const FHitResult& SweepResult)
+void AScream::WaveOverlap(AActor* OtherActor)
 {
     APlayerPawn* PlayerPawn = Cast<APlayerPawn>(OtherActor);
     if (PlayerPawn)
     {
         FVector DragDirection = PlayerPawn->GetActorLocation() - GetActorLocation();
         DragDirection.Normalize();
-        PlayerPawn->GetPlaneMovement()->DragMovementEffect->Activate(FirstWaveDragForce, DragDirection);
+        // Dragging or impulsing the player aways lel
+        if(bDragOrImpulse)
+        {
+            PlayerPawn->GetPlaneMovement()->DragMovementEffect->Activate(FirstWaveDragForce, DragDirection);
+        }
+        else
+        {
+            PlayerPawn->GetPlaneBox()->AddImpulse(DragDirection * 9000.f, FName(), true);
+            SecondWaveMesh->SetCollisionProfileName(TEXT("IgnoreAll"));
+        }
     }
 }
 
-void AScream::SecondWaveOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
-                                UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
-                                bool bFromSweep, const FHitResult& SweepResult)
+
+void AScream::WaveOverlapEnd(AActor* OtherActor)
 {
-    APlayerPawn* PlayerPawn = Cast<APlayerPawn>(OtherActor);
-    if (PlayerPawn)
+    if(bDragOrImpulse)
     {
-        FVector DragDirection = PlayerPawn->GetActorLocation() - GetActorLocation();
-        DragDirection.Normalize();
-        PlayerPawn->GetPlaneBox()->AddImpulse(DragDirection * 9000.f, FName(), true);
-        SecondWaveMesh->SetCollisionProfileName(TEXT("IgnoreAll"));
+        APlayerPawn* PlayerPawn = Cast<APlayerPawn>(OtherActor);
+        if (PlayerPawn)
+        {
+            PlayerPawn->GetPlaneMovement()->DragMovementEffect->Deactivate();
+        }
     }
 }
 
-
-void AScream::FirstWaveOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
-                                  UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
-{
-    APlayerPawn* PlayerPawn = Cast<APlayerPawn>(OtherActor);
-    if (PlayerPawn)
-    {
-        PlayerPawn->GetPlaneMovement()->DragMovementEffect->Deactivate();
-    }
-}
-
-void AScream::SecondWaveOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
-                                   UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
-{
-}
-
-void AScream::SecondWaveDelayStart()
-{
-    DidSecondWaveStart = true;
-}
--- Lutenium/Source/Lutenium/public/Monster/Weapons/Scream.h
+++ Lutenium/Source/Lutenium/public/Monster/Weapons/Scream.h
@@ -19,10 +19,7 @@ public:
     class USphereComponent* MainSphereComp;
 
     UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category=Mesh)
-    class UStaticMeshComponent* FirstWaveMesh;
-
-    UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category=Mesh)
-    class UStaticMeshComponent* SecondWaveMesh;
+    class UStaticMeshComponent* WaveMesh;
 
     UPROPERTY(EditDefaultsOnly, Category=Damage)
     float Damage;
@@ -30,26 +27,15 @@ public:
     UPROPERTY(EditDefaultsOnly, Category=Damage)
     float ExpansionSpeed;
 
+    UPROPERTY(EditDefaultsOnly, Category=Damage)
+    bool bDragOrImpulse; // true - dragging the player, false - impulse the player away
+
 
     UFUNCTION(BlueprintCallable)
-    void FirstWaveOverlap(
-        class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor,
-        class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
-        bool bFromSweep, const FHitResult& SweepResult);
+    void WaveOverlap(class AActor* OtherActor);
 
     UFUNCTION(BlueprintCallable)
-    void SecondWaveOverlap(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor,
-                           class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
-                           bool bFromSweep, const FHitResult& SweepResult
-    );
-
-    UFUNCTION(BlueprintCallable)
-    void FirstWaveOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor,
-                             class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
-
-    UFUNCTION(BlueprintCallable)
-    void SecondWaveOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor,
-                              class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+    void WaveOverlapEnd(class AActor* OtherActor);
 
     UPROPERTY(EditDefaultsOnly,BlueprintReadWrite)
     FVector ScalingMultiplier;
@@ -58,24 +44,17 @@ public:
     float ScalingSpeed;
 
     UPROPERTY(EditDefaultsOnly, Category="Waves")
-    float SecondFaveDelay;
-
-    UPROPERTY(EditDefaultsOnly, Category="Waves")
     float WavesLifeSpan;
 
     UPROPERTY(EditDefaultsOnly, Category="Waves")
-    float FirstWaveForceImpact;
+    float ForceImpact;
 
     UPROPERTY(EditDefaultsOnly, Category="Waves")
-    float FirstWaveDragForce;
+    float DragForce;
 protected:
     virtual void BeginPlay() override;
 
-private:
 
-    void SecondWaveDelayStart();
-
-    bool DidSecondWaveStart;
 public:
     virtual void Tick(float DeltaTime) override;
 
