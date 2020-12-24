69be82a911528375fe8bd39d4b2cdc1e0d916e8c
Misha <homakhaker@gmail.com>
2020-12-24 22:04:25 +0100

    creating a fancy fan

--- Lutenium/Source/Lutenium/private/Monster/Weapons/FanMW.cpp
+++ Lutenium/Source/Lutenium/private/Monster/Weapons/FanMW.cpp
@@ -1,12 +1,30 @@
 #include "Monster/Weapons/FanMW.h"
-#include "Monster/Weapons/Spells/Emp.h"
 #include "Monster/EnemyMonsterPawn.h"
 #include "Monster/Weapons/WeaponsUtils/MonsterWeaponType.h"
+#include "Player/PlayerPawn.h"
+#include "Player/Missile.h"
+#include "Player/PlaneMovementComponent.h"
+#include "Player/MovementEffect/DragMovementEffect.h"
 #include "GameFramework/Actor.h"
+#include "Components/StaticMeshComponent.h"
 #include "Components/PrimitiveComponent.h"
 
 AFanMW::AFanMW() : AMonsterWeapon()
 {
+    /* Setting up mesh collisions */
+    ForwardWindCollision = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Forward wind mesh"));
+    BackwardWindCollision = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Backward wind mesh"));
+    ForwardWindCollision->AttachToComponent(WeaponMesh, FAttachmentTransformRules::KeepWorldTransform);
+    BackwardWindCollision->AttachToComponent(WeaponMesh, FAttachmentTransformRules::KeepWorldTransform);
+    ForwardWindCollision->SetCollisionProfileName(TEXT("OverlapAll"));
+    BackwardWindCollision->SetCollisionProfileName(TEXT("OverlapAll"));
+
+    /* Forces */
+    PushForce = 9000.f;
+    PullForce = 9000.f;
+
+    WindActiveTime = 5.f;
+
     Health = 100.f;
 
     WeaponType = EMonsterWeaponType::Fan;
@@ -14,20 +32,52 @@ AFanMW::AFanMW() : AMonsterWeapon()
 
 void AFanMW::ExecuteAttack()
 {
-    if (EmpClass)
+    bForwardWindActive = true;
+    bBackWindActive = true;
+
+    /* Invoking events for visuals n shit */
+    OnActivateForwardWind(true);
+    OnActivateForwardWind(false);
+
+    /* After WindActiveTime deactivate winds */
+    FTimerHandle WindTimeHandler;
+    GetWorldTimerManager().SetTimer(WindTimeHandler, this, &AFanMW::ExecuteAttack, WindActiveTime, false);
+}
+
+/** Handles overlaped player and missile */
+void AFanMW::WindTriggerOverlap(class AActor* OtherActor, bool bPush)
+{
+    /* Player has overlaped */
+    APlayerPawn* PlayerPawn = Cast<APlayerPawn>(OtherActor);
+    if (PlayerPawn)
     {
-        UWorld *World = GetWorld();
-        if (World)
-        {
-            FActorSpawnParameters SpawnParams;
-            SpawnParams.Owner = MonsterPawn;
-            SpawnParams.Instigator = MonsterPawn;
-            AEmp *Emp = World->SpawnActor<AEmp>(EmpClass, WeaponMesh->GetSocketLocation(EmpSocket),
-                                                WeaponMesh->GetSocketRotation(EmpSocket), SpawnParams);
-        }
+        FVector ForceDirection = PlayerPawn->GetActorLocation() - GetActorLocation();
+        ForceDirection.Normalize();
+        /* Pushing away player or pulling it towards */
+        ForceDirection = bPush ? ForceDirection : ForceDirection * -1.f;
+        const float Force = bPush ? PushForce : PullForce;
+        PlayerPawn->GetPlaneMovement()->DragMovementEffect->Activate(Force, DragDirection);
+        return;
+    }
+
+    /* Missile has overlaped */
+    AMissile* Missile = Cast<AMissile>(OtherActor);
+    if(Missile)
+    {
+        /* Throws missile in the opposite direction */
+        Position = Missile->GetActorLocation() - Position;
+        Position.Normalize();
+        Missile->ThrowMissile(Missile->GetActorForwardVector() * -1, PushForce);
+        /* Do not defected the missile */
+        return;
     }
 }
 
-void AFanMW::DoShoot()
+void AFanMW::WindTriggerOverlapEnd(class AActor* OtherActor)
 {
+    APlayerPawn* PlayerPawn = Cast<APlayerPawn>(OtherActor);
+    if (PlayerPawn)
+    {
+        PlayerPawn->GetPlaneMovement()->DragMovementEffect->Deactivate();
+    }
 }
--- Lutenium/Source/Lutenium/public/Monster/Weapons/FanMW.h
+++ Lutenium/Source/Lutenium/public/Monster/Weapons/FanMW.h
@@ -7,7 +7,10 @@
 #include "FanMW.generated.h"
 
 /**
- *
+ * Big ass fan weapon
+ * Settels on the middle hight level of the monster
+ * Normal attack - activate force pushing away in forward direction
+ * Upgraded attack - two waves force (pushing and pulling)
  */
 UCLASS()
 class LUTENIUM_API AFanMW : public AMonsterWeapon
@@ -19,13 +22,103 @@ public:
 
     virtual void ExecuteAttack() override;
 
-    UFUNCTION(BlueprintCallable)
-    void DoShoot();
+    // ------------------------------------------------------------------
+	// Wind collisions
+	// ------------------------------------------------------------------
 
-    UPROPERTY(EditDefaultsOnly, Category = "Emp")
-	TSubclassOf<class AEmp> EmpClass;
+    /**
+     * Forward mesh to create a wind collision
+     */
+    UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category= "Wind")
+    class UStaticMeshComponent* ForwardWindCollision;
 
-    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly,  Category = "Emp")
-    FName EmpSocket;
+    /**
+     * Backward mesh to create a wind collision
+     */
+    UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category= "Wind")
+    class UStaticMeshComponent* BackwardWindCollision;
 
+    /**
+     * Function called on BeginOverlap event
+     * When player/missle enter the mesh collision, pushing it away or pulling it in depending on the bPush
+     * @param OtherActor - actor that entered the collision
+     * @param bPush - determins if the wind should push away or pull towards the fan
+     */
+    UFUNCTION(BlueprintCallable, Category= "Wind")
+    void WindTriggerOverlap(class AActor* OtherActor, bool bPush);
+
+    /**
+     * Function called on ExitOverlap event
+     * When player exit the mesh collision, deactivating movement effects
+     * @param OtherActor - actor that entered the collision
+     */
+    UFUNCTION(BlueprintCallable, Category= "Wind")
+    void WindTriggerOverlapEnd(class AActor* OtherActor);
+
+    /**
+     * Should forward wind be active?
+     */
+    UPROPERTY(BlueprintReadWrite, Category= "Wind")
+    bool bForwardWindActive;
+
+    /**
+     * Should backward wind be active?
+     */
+    UPROPERTY(BlueprintReadWrite, Category= "Wind")
+    bool bBackwardWindActive;
+
+    /**
+     * How long should wind be active for
+     */
+    UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category= "Wind")
+    float WindActiveTime;
+
+    /**
+     * Deactives wind after WindActiveTime has passed
+     * Deactives both forward and backward wind.
+     */
+    FORCEINLINE UFUNCTION(BlueprintCallable, Category= "Wind")
+    void DeactivateWind()
+    {
+        /* Invoking events for visuals n shit */
+        if(bForwardWindActive)
+        {
+            OnDeactivateForwardWind(true);
+        }
+        if(bBackWindActive)
+        {
+            OnDeactivateForwardWind(true);
+        }
+
+        bForwardWindActive = false;
+        bBackWindActive = false;
+    }
+
+    /**
+     * Event on wind get actived
+     * Do the particle shit in bp
+     * @param bForward - true if that's forward wind, false if its backward
+     */
+    UFUNCTION(BlueprintImplementableEvent, Category="Wind")
+    void OnActivateForwardWind(bool& bForward);
+
+    /**
+     * Event on wind get deactivated
+     * Do the particle shit in bp
+     * @param bForward - true if that's forward wind, false if its backward
+     */
+    UFUNCTION(BlueprintImplementableEvent, Category="Wind")
+    void OnDeactivateForwardWind(bool& bForward);
+
+    /**
+     * Push force amount applied to the mesh on the player that overlaps wind
+     */
+    UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category= "Wind")
+    float PushForce;
+
+    /**
+     * Pull force amount applied to the mesh on the player that overlaps wind
+     */
+    UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category= "Wind")
+    float PullForce;
 };
