40180e99803e994371464663dce5f5098eff3701
Misha <homakhaker@gmail.com>
2021-03-30 21:07:22 +0200

    fixed after windows reinstal

--- Vars.bat
+++ Vars.bat
@@ -7,6 +7,6 @@ set PROJECT=Lutenium
 set PROJECT_DIR=%ROOTDIR%\%PROJECT%
 set UPROJECT_PATH=%PROJECT_DIR%\%PROJECT%.uproject
 
-set UE4_DIR=O:\Programs\Unreal Engine\UE_4.26
+set UE4_DIR=O:\Programs\UnrealEngine4\UE_4.26
 set UE4EDITOR_EXE=%UE4_DIR%\Engine\Binares\Win64\UE4Editor.exe
 set BUILD_PATH=%UE4_DIR%\Engine\Build\BatchFiles\Build.bat
